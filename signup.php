6685b9cd78e96eee6db15bbcfe5696fee8025088
delphine <delphin38@live.fr>
2023-12-05 20:53:24 +0100

    création fichier connexion

--- signup.php
+++ signup.php
@@ -33,6 +33,7 @@
         </div>
         
         <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
+        <a href="login.php"><p>J'ai déjà un compte, je me connecte</p></a>
         <?php   
         ?>
 
